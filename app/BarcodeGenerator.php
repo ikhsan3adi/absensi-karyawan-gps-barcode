34c0b31a1f9f452c2bd0680198fcfecd2fb198d5
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-06-18 19:26:26 +0700

    Update dependencies

--- app/BarcodeGenerator.php
+++ app/BarcodeGenerator.php
@@ -6,65 +6,22 @@ use Endroid\QrCode\Color\Color;
 use Endroid\QrCode\ErrorCorrectionLevel;
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
-use Endroid\QrCode\RoundBlockSizeMode;
 use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
-use Picqer\Barcode\BarcodeGeneratorPNG;
 
 class BarcodeGenerator
 {
-
     /**
      * Create a new class instance.
      */
     public function __construct(
-        protected $barcodeGenerator = new BarcodeGeneratorPNG(),
         protected $qrGenerator = new QrCode(''),
         protected $writer = new PngWriter(),
         protected $manager = new ImageManager(Driver::class),
-        protected $barcodeHeight = 75,
         protected $width = 720,
         protected $height = 720,
     ) {
         //
-    }
-
-    /**
-     * @deprecated
-     */
-    public function generateBarcode($value)
-    {
-        $barcodeFile = $this->barcodeGenerator->getBarcode(
-            $value,
-            type: $this->barcodeGenerator::TYPE_CODE_128,
-            height: $this->barcodeHeight,
-        );
-
-        return $this->manager->create($this->width, $this->height)
-            ->fill('#fff')
-            ->place($this->manager->read($barcodeFile)->scale($this->width * .9), 'center')
-            ->toPng();
-    }
-
-    /**
-     * @deprecated
-     * @param array<string, string> $values name => value
-     */
-    public function generateBarcodesZip(array $values)
-    {
-        $zip = new \ZipArchive();
-        $dir = public_path('temp');
-        if (!file_exists($dir)) {
-            mkdir($dir, recursive: true);
-        }
-        $zipFile = public_path('temp/barcodes.zip');
-        $zip->open($zipFile, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
-        foreach ($values as $name => $value) {
-            $barcodeFile = $this->generateBarcode($value);
-            $zip->addFromString(($name ?? $value) . '.png', $barcodeFile->toString());
-        }
-        $zip->close();
-        return $zipFile;
     }
 
     public function generateQrCode($value)
@@ -72,9 +29,8 @@ class BarcodeGenerator
         $qrCode = $this->qrGenerator
             ->create($value)
             ->setSize(300)
-            ->setMargin(10)
+            ->setMargin(20)
             ->setErrorCorrectionLevel(ErrorCorrectionLevel::Medium)
-            ->setRoundBlockSizeMode(RoundBlockSizeMode::Margin)
             ->setForegroundColor(new Color(0, 0, 0))
             ->setBackgroundColor(new Color(255, 255, 255));
 
@@ -82,7 +38,7 @@ class BarcodeGenerator
 
         return $this->manager->create($this->width, $this->height)
             ->fill('#fff')
-            ->place($this->manager->read($file)->scale($this->width * .9), 'center')
+            ->place($this->manager->read($file)->scale($this->width), 'center')
             ->toPng();
     }
 
