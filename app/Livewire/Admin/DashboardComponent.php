2fae4dc5a7d7da097dc8c619227eeb4d0ba7b60a
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-06-25 11:45:22 +0700

    Admin dashboard feature

---
+++ app/Livewire/Admin/DashboardComponent.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use App\Models\Attendance;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Component;
+
+class DashboardComponent extends Component
+{
+    public bool $showDetail = false;
+    public $currentAttendance = [];
+
+    public function show($attendanceId)
+    {
+        /** @var Attendance */
+        $attendance = Attendance::find($attendanceId);
+        if ($attendance) {
+            $this->showDetail = true;
+            $this->currentAttendance = $attendance->getAttributes();
+            $this->currentAttendance['coordinates'] = $attendance->lat_lng;
+            $this->currentAttendance['name'] = $attendance->user->name;
+            $this->currentAttendance['nip'] = $attendance->user->nip;
+            if ($attendance->attachment) {
+                $this->currentAttendance['attachment'] = $attendance->attachment_url;
+            }
+            if ($attendance->barcode_id) {
+                $this->currentAttendance['barcode'] = $attendance->barcode;
+            }
+        }
+    }
+
+    public function render()
+    {
+        /** @var Collection<Attendance>  */
+        $attendances = Attendance::where('date', date('Y-m-d'))->get();
+
+        /** @var Collection<User>  */
+        $employees = User::where('group', 'user')
+            ->paginate(20)
+            ->through(function (User $user) use ($attendances) {
+                return $user->setAttribute(
+                    'attendance',
+                    $attendances
+                        ->where(fn (Attendance $attendance) => $attendance->user_id === $user->id)
+                        ->first(),
+                );
+            });
+
+        $employeesCount = User::where('group', 'user')->count();
+        $presentCount = $attendances->where(fn ($attendance) => $attendance->status === 'present')->count();
+        $lateCount = $attendances->where(fn ($attendance) => $attendance->status === 'late')->count();
+        $excusedCount = $attendances->where(fn ($attendance) => $attendance->status === 'excused')->count();
+        $sickCount = $attendances->where(fn ($attendance) => $attendance->status === 'sick')->count();
+        $absentCount = $attendances->where(fn ($attendance) => $attendance->status === 'absent')->count();
+
+        return view('livewire.admin.dashboard', [
+            'employees' => $employees,
+            'employeesCount' => $employeesCount,
+            'presentCount' => $presentCount,
+            'lateCount' => $lateCount,
+            'excusedCount' => $excusedCount,
+            'sickCount' => $sickCount,
+            'absentCount' => $absentCount,
+        ]);
+    }
+}
---
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -0,0 +1,242 @@
+@php
+  $date = Carbon\Carbon::now();
+@endphp
+<div>
+  @pushOnce('styles')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+  @endpushOnce
+  <div class="flex flex-col justify-between sm:flex-row">
+    <h3 class="mb-4 text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
+      Absensi Hari Ini
+    </h3>
+    <h3 class="mb-4 text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
+      Jumlah Karyawan: {{ $employeesCount }}
+    </h3>
+  </div>
+  <div class="mb-4 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
+    <div class="rounded-md bg-green-200 px-8 py-4 text-gray-800 dark:bg-green-900 dark:text-white dark:shadow-gray-700">
+      <span class="text-2xl font-semibold md:text-3xl">Hadir: {{ $presentCount }}</span><br>
+      <span>Terlambat: {{ $lateCount }}</span>
+    </div>
+    <div class="rounded-md bg-blue-200 px-8 py-4 text-gray-800 dark:bg-blue-900 dark:text-white dark:shadow-gray-700">
+      <span class="text-2xl font-semibold md:text-3xl">Izin: {{ $excusedCount }}</span><br>
+      <span>Izin/Cuti</span>
+    </div>
+    <div
+      class="rounded-md bg-purple-200 px-8 py-4 text-gray-800 dark:bg-purple-900 dark:text-white dark:shadow-gray-700">
+      <span class="text-2xl font-semibold md:text-3xl">Sakit: {{ $sickCount }}</span>
+    </div>
+    <div class="rounded-md bg-red-200 px-8 py-4 text-gray-800 dark:bg-red-900 dark:text-white dark:shadow-gray-700">
+      <span class="text-2xl font-semibold md:text-3xl">Tidak Hadir: {{ $absentCount }}</span><br>
+      <span>Tidak/Belum Hadir</span>
+    </div>
+  </div>
+
+  <div class="mb-4 overflow-x-scroll">
+    <table class="w-full divide-y divide-gray-200 dark:divide-gray-700">
+      <thead class="bg-gray-50 dark:bg-gray-900">
+        <tr>
+          <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">
+            {{ __('Name') }}
+          </th>
+          <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">
+            {{ __('NIP') }}
+          </th>
+          <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">
+            {{ __('Division') }}
+          </th>
+          <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">
+            {{ __('Job Title') }}
+          </th>
+          <th scope="col"
+            class="text-nowrap border border-gray-300 px-1 py-3 text-center text-xs font-medium text-gray-500 dark:border-gray-600 dark:text-gray-300">
+            Status
+          </th>
+          <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">
+            {{ __('Time In') }}
+          </th>
+          <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300">
+            {{ __('Time Out') }}
+          </th>
+          <th scope="col" class="relative">
+            <span class="sr-only">Actions</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-gray-200 bg-white dark:divide-gray-700 dark:bg-gray-800">
+        @php
+          $class = 'px-4 py-3 text-sm font-medium text-gray-900 dark:text-white';
+        @endphp
+        @foreach ($employees as $employee)
+          @php
+            $attendance = $employee->attendance;
+            $timeIn = $attendance ? $attendance?->time_in?->format('H:i:s') : null;
+            $timeOut = $attendance ? $attendance?->time_out?->format('H:i:s') : null;
+            $isWeekend = $date->isWeekend();
+            $status = ($attendance ?? [
+                'status' => $isWeekend || !$date->isPast() ? '-' : 'absent',
+            ])['status'];
+            switch ($status) {
+                case 'present':
+                    $shortStatus = 'H';
+                    $bgColor =
+                        'bg-green-200 dark:bg-green-800 hover:bg-green-300 dark:hover:bg-green-700 border border-green-300 dark:border-green-600';
+                    break;
+                case 'late':
+                    $shortStatus = 'T';
+                    $bgColor =
+                        'bg-amber-200 dark:bg-amber-800 hover:bg-amber-300 dark:hover:bg-amber-700 border border-amber-300 dark:border-amber-600';
+                    break;
+                case 'excused':
+                    $shortStatus = 'I';
+                    $bgColor =
+                        'bg-blue-200 dark:bg-blue-800 hover:bg-blue-300 dark:hover:bg-blue-700 border border-blue-300 dark:border-blue-600';
+                    break;
+                case 'sick':
+                    $shortStatus = 'S';
+                    $bgColor = 'hover:bg-gray-100 dark:hover:bg-gray-700 border border-gray-300 dark:border-gray-600';
+                    break;
+                case 'absent':
+                    $shortStatus = 'A';
+                    $bgColor =
+                        'bg-red-200 dark:bg-red-800 hover:bg-red-300 dark:hover:bg-red-700 border border-red-300 dark:border-red-600';
+                    break;
+                default:
+                    $shortStatus = '-';
+                    $bgColor = 'hover:bg-gray-100 dark:hover:bg-gray-700 border border-gray-300 dark:border-gray-600';
+                    break;
+            }
+          @endphp
+          <tr wire:key="{{ $employee->id }}" class="group">
+            {{-- Detail karyawan --}}
+            <td class="{{ $class }} text-nowrap group-hover:bg-gray-100 dark:group-hover:bg-gray-700">
+              {{ $employee->name }}
+            </td>
+            <td class="{{ $class }} group-hover:bg-gray-100 dark:group-hover:bg-gray-700">
+              {{ $employee->nip }}
+            </td>
+            <td class="{{ $class }} text-nowrap group-hover:bg-gray-100 dark:group-hover:bg-gray-700">
+              {{ $employee->division?->name ?? '-' }}
+            </td>
+            <td class="{{ $class }} text-nowrap group-hover:bg-gray-100 dark:group-hover:bg-gray-700">
+              {{ $employee->jobTitle?->name ?? '-' }}
+            </td>
+
+            {{-- Absensi --}}
+            <td
+              class="{{ $bgColor }} text-nowrap px-1 py-3 text-center text-sm font-medium text-gray-900 dark:text-white">
+              {{ __($status) }}
+            </td>
+
+            {{-- Waktu masuk/keluar --}}
+            <td class="{{ $class }} group-hover:bg-gray-100 dark:group-hover:bg-gray-700">
+              {{ $timeIn ?? '-' }}
+            </td>
+            <td class="{{ $class }} group-hover:bg-gray-100 dark:group-hover:bg-gray-700">
+              {{ $timeOut ?? '-' }}
+            </td>
+
+            {{-- Action --}}
+            <td
+              class="cursor-pointer text-center text-sm font-medium text-gray-900 group-hover:bg-gray-100 dark:text-white dark:group-hover:bg-gray-700">
+              <div class="flex items-center justify-center gap-3">
+                @if ($attendance && ($attendance->attachment || $attendance->note || $attendance->coordinates))
+                  <x-button type="button" wire:click="show({{ $attendance->id }})"
+                    onclick="setLocation({{ $attendance->lat_lng['lat'] ?? 0 }}, {{ $attendance->lat_lng['lng'] ?? 0 }})">
+                    {{ __('Detail') }}
+                  </x-button>
+                @else
+                  -
+                @endif
+              </div>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+  {{ $employees->links() }}
+
+  <x-modal wire:model="showDetail" onclose="removeMap()">
+    <div class="px-6 py-4">
+      @if ($currentAttendance)
+        <h3 class="mb-3 text-xl font-semibold">{{ $currentAttendance['name'] }}</h3>
+        <div class="mb-3 w-full">
+          <x-label for="nip" value="{{ __('NIP') }}"></x-label>
+          <x-input type="text" class="w-full" id="nip" disabled
+            value="{{ $currentAttendance['nip'] }}"></x-input>
+        </div>
+        <div class="mb-3 flex w-full gap-3">
+          <div class="w-full">
+            <x-label for="date" value="{{ __('Date') }}"></x-label>
+            <x-input type="text" class="w-full" id="date" disabled
+              value="{{ $currentAttendance['date'] }}"></x-input>
+          </div>
+          <div class="w-full">
+            <x-label for="status" value="{{ __('Status') }}"></x-label>
+            <x-input type="text" class="w-full" id="status" disabled
+              value="{{ __($currentAttendance['status']) }}"></x-input>
+          </div>
+        </div>
+        <div class="flex flex-col gap-3">
+          @if ($currentAttendance['attachment'])
+            <x-label for="attachment" value="{{ __('Attachment') }}"></x-label>
+            <img src="{{ $currentAttendance['attachment'] }}" alt="Attachment"
+              class="max-h-64 object-contain md:max-h-96">
+          @endif
+          @if ($currentAttendance['note'])
+            <x-label for="note" value="Keterangan"></x-label>
+            <x-textarea type="text" id="note" disabled value="{{ $currentAttendance['note'] }}"></x-textarea>
+          @endif
+          @if (
+              $currentAttendance['coordinates'] &&
+                  $currentAttendance['coordinates']['lat'] &&
+                  $currentAttendance['coordinates']['lng']
+          )
+            <x-label for="map" value="Koordinat Lokasi Absen"></x-label>
+            <p>{{ $currentAttendance['coordinates']['lat'] }}, {{ $currentAttendance['coordinates']['lng'] }}</p>
+            <div class="my-2 h-52 w-full md:h-64" id="map"></div>
+          @endif
+          @if ($currentAttendance['time_in'] || $currentAttendance['time_out'])
+            <div class="grid grid-cols-2 gap-3">
+              <x-label for="time_in" value="Waktu Masuk"></x-label>
+              <x-label for="time_out" value="Waktu Keluar"></x-label>
+              <x-input type="text" id="time_in" disabled
+                value="{{ $currentAttendance['time_in'] ?? '-' }}"></x-input>
+              <x-input type="text" id="time_out" disabled
+                value="{{ $currentAttendance['time_out'] ?? '-' }}"></x-input>
+            </div>
+          @endif
+
+          @if ($currentAttendance['barcode'] ?? false)
+            <x-label for="barcode" value="Barcode"></x-label>
+            <x-input type="text" id="barcode" disabled
+              value="{{ $currentAttendance['barcode']['name'] }}"></x-input>
+          @endif
+        </div>
+      @endif
+    </div>
+  </x-modal>
+  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+  <script>
+    let map = null;
+
+    function setLocation(lat, lng) {
+      removeMap();
+      setTimeout(() => {
+        map = L.map('map').setView([Number(lat), Number(lng)], 19);
+        L.marker([Number(lat), Number(lng)]).addTo(map);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 21,
+        }).addTo(map);
+      }, 500);
+    }
+
+    function removeMap() {
+      if (map !== null) map.remove();
+      map = null;
+    }
+  </script>
+</div>
