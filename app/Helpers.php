f3dbd6fcfd426cb6f4b3363bc4a8014384ff3df7
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-06-28 14:27:13 +0700

    Update Helpers.php

--- app/Helpers.php
+++ app/Helpers.php
@@ -17,11 +17,13 @@ class Helpers
      *
      * @param mixed $point The point to unpack.
      * @return array The unpacked point in the format:
-     *               [
-     *                   ...
-     *                   'lat' => latitude,
-     *                   'lng' => longitude
-     *               ]
+     * ```
+     * [
+     *   ...
+     *   'lat' => latitude,
+     *   'lng' => longitude
+     * ]
+     * ```
      */
     public static function unpackPoint($point = null)
     {
@@ -36,6 +38,13 @@ class Helpers
         return "https://maps.google.com/maps?q=$lat,$lng";
     }
 
+    /**
+     * Get the year and week formatted as a string in the ISO 8601 week date format.
+     *
+     * @param string|int $week The week number to format.
+     * @param string|int $year The year to format.
+     * @return string The formatted year-week string.
+     */
     public static function yearWeekString(string|int $week, string|int $year): string
     {
         if ($week < 10 && !str_contains($week, '0')) {
